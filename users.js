cafbd18b27e4faebb34e36e4b06ca8e029df8453
Matt Lane <lane.matthew@gmail.com>
2020-03-23 09:58:42 -0700

    great feature added

--- users.js
+++ users.js
@@ -1,6 +1,9 @@
 const users = ["matt"];
-users.push("Nate", "Alissa");
+users.push("Nate", "Alissa", "someone else");
 
 const yellingUsers = users.map(u => u.toUpperCase());
+
+const randomThing = Math.random();
+console.log("this is a weird feature");
 
 module.exports = { users, yellingUsers };
