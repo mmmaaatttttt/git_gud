480c2794bd114c389edb4f610cad4d1643c74d6f
Matt Lane <lane.matthew@gmail.com>
2020-03-23 09:55:46 -0700

    more changes to users.js

--- users.js
+++ users.js
@@ -1,6 +1,9 @@
 const users = ["matt"];
-users.push("Nate", "Alissa");
+users.push("Nate", "Alissa", "Elie", "Joel", "Tim");
 
 const yellingUsers = users.map(u => u.toUpperCase());
 
-module.exports = { users, yellingUsers };
+const r14Instructors = users.slice(0, 3);
+const r15Instructors = users.slice(3);
+
+module.exports = { users, yellingUsers, r14Instructors, r15Instructors };
