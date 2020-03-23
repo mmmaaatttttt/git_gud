480c6f4273da1d4be6ac4f5baf9db28b506e0184
Matt Lane <lane.matthew@gmail.com>
2020-03-23 10:00:32 -0700

    fixed merge conflict

--- users.js
+++ users.js
@@ -1,5 +1,4 @@
 const users = ["matt"];
-<<<<<<< HEAD
 users.push("Nate", "Alissa", "Elie", "Joel", "Tim");
 
 const yellingUsers = users.map(u => u.toUpperCase());
@@ -7,14 +6,6 @@ const yellingUsers = users.map(u => u.toUpperCase());
 const r14Instructors = users.slice(0, 3);
 const r15Instructors = users.slice(3);
 
-module.exports = { users, yellingUsers, r14Instructors, r15Instructors };
-=======
-users.push("Nate", "Alissa", "someone else");
-
-const yellingUsers = users.map(u => u.toUpperCase());
-
-const randomThing = Math.random();
 console.log("this is a weird feature");
 
-module.exports = { users, yellingUsers };
->>>>>>> master
+module.exports = { users, yellingUsers, r14Instructors, r15Instructors };
